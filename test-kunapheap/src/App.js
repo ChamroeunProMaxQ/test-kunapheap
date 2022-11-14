53210ad7d483c2dba19a8f711624cfde9c498688
ChamroeunProMaxQ <stevenkakoreal@gmail.com>
2022-11-15 00:01:53 +0700

    add new logo

--- test-kunapheap/src/App.js
+++ test-kunapheap/src/App.js
@@ -1,19 +1,27 @@
 import "./App.css";
 import { Helmet } from "react-helmet";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Team from "./components/Team";
 import Home from "./components/Home";
+import logo168 from "./assets/168.png";
 
 function App() {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Kunapheap Store</title>
-      </Helmet>
+      <div>
+        <Helmet>
+          <title>Kunapheap Store</title>
+          <meta charSet="utf-8" />
+          <meta property="og:title" content="Kunapheap Store" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://kunapheap.com/" />
+          <meta property="og:image" content={logo168} />
+        </Helmet>
+      </div>
+
       <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/team" element={<Team/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
     </>
   );
--- test-kunapheap/src/components/Home.js
+++ test-kunapheap/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../App.css"
 import {Link } from 'react-router-dom'
-import logo from "../assets/shopingcart.gif";
+import logo from "../assets/kunapheaplogo.svg";
 
 function Home() {
   return (
@@ -9,10 +9,9 @@ function Home() {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div className="line">____________________________________</div>
-          <p className="under-text">Kunapheap Store Coming Soon ....</p>
-          <h3>Developing By </h3>
-          <Link to="/team" ><h1 className='no-margin'>Team 168 </h1></Link>
+          <p className="under-text" style={{fontWeight: 'bolder'}}>Kunapheap is Quality Coming Soon ..</p>
+          <h3 style={{color: "black"}}>Developing By </h3>
+          <Link to="/team" ><h1 className='no-margin' style={{color: "#7DBE7C", textDecoration: 'none'}}>Team 168 </h1></Link>
         </header>
       </div>
     </div>
